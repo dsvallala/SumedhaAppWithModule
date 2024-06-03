2769c6d6f65e47dca7ff04595ce5d76c09afbfba
dsvallala <sumedhanakka98@gmail.com>
2024-06-03 18:20:17 -0400

    Forms added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
   title = 'SumedhaAppWithModule';
+userForm: any;
 }
